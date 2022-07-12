0d93c24b2003b4479cef9d4284fe7a3fce18bfb4
brilik <megabrilik@gmail.com>
2022-07-12 23:13:49 +0300

    Added maps for assets

--- gulpfile.js
+++ gulpfile.js
@@ -2,26 +2,31 @@ const gulp = require('gulp'),
     sass = require('gulp-sass')(require('sass')),
     minifyCss = require('gulp-minify-css'),
     minify = require('gulp-minify'),
-    rename = require('gulp-rename')
+    rename = require('gulp-rename'),
+    sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('css', function () {
     return gulp.src('./assets/css/**/*.scss')
+        .pipe(sourcemaps.init())
         .pipe(sass())
         .pipe(minifyCss({
             keepSpecialComments: 0,
         }))
         .pipe(rename({extname: '.min.css'}))
+        .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest('./assets/css/'))
 });
 
 gulp.task('js', function () {
         return gulp.src('./assets/js/**/!(*.min)*.js')
+        .pipe(sourcemaps.init())
         .pipe(minify({
             ext: {
                 min: '.min.js',
             },
             noSource: true,
         }))
+        .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest('./assets/js/'))
 });
 
